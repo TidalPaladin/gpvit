e63b0fb79fe9e63094399962fcdc36de7c3794ab
Scott Chase Waggener <tidalpaladin@protonmail.com>
2023-11-05 10:05:03 -0600

    Add MLPMixer pooling layer

--- gpvit/layers.py
+++ gpvit/layers.py
@@ -329,3 +329,41 @@ class GroupPropagationTransformer(GroupPropagation):
             ),
             mixer_repeats,
         )
+
+
+class MLPMixerPooling(MLPMixer):
+    """
+    MLP Mixer pooling head for group propagation tokens.
+
+    Args:
+        dim: The dimension of the input tensor.
+        token_dim: The hidden dimension of the token-mixing MLP.
+        num_patches: The number of patches to be considered for token mixing.
+        channel_dim: The hidden dimension of the channel-mixing MLP.
+        dropout: Dropout rate for the MLPs. Defaults to 0.0.
+        activation: Activation function for the MLPs. Defaults to nn.GELU().
+        output_tokens: The number of output tokens. Defaults to 1.
+    """
+
+    def __init__(
+        self,
+        dim: int,
+        token_dim: int,
+        num_patches: int,
+        channel_dim: int,
+        dropout: float = 0.0,
+        activation: nn.Module = nn.GELU(),
+        output_tokens: int = 1,
+    ):
+        super().__init__(dim, token_dim, num_patches, channel_dim, dropout, activation)
+        self.token_mixing = nn.Sequential(
+            nn.Conv1d(num_patches, token_dim, 1),
+            copy(activation),
+            nn.Dropout(dropout),
+            nn.Conv1d(token_dim, output_tokens, 1),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.norm1(self.token_mixing(x))
+        x = self.norm2(x + self.channel_mixing(x))
+        return x
